03ccdb0cd4132862675eadd8719791aba7ab9780
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2024-05-06 12:33:07 +0545

    Updated

--- src/Component/LandingPage/HowITWorks.jsx
+++ src/Component/LandingPage/HowITWorks.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import img from "../Assets/iphone.png";
+import img from "../../Assets/Page1/iphone.png";
 
 function HowItWorks() {
   return (
